75d73cf95b1566b7e081862ba1af1a3ee8ae28bb
Muresan Andrei <murimoro67@gmail.com>
2024-05-01 20:29:21 +0300

    Added Splash

---
+++ lib/splash_page.dart
@@ -0,0 +1,55 @@
+import 'package:app_first_may/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  void _navigatetohome() async {
+    await Future.delayed(const Duration(milliseconds: 2500), () {});
+    if(!mounted) return;
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => const LoginPage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff121212),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 150,
+              height: 150,
+              child: SvgPicture.asset('assets/images/star.svg'),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 13.0),
+              child: Text('StarBank',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontFamily: 'Poppins')),
+            )
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
