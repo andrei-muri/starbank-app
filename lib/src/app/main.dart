be4ec775a758eee1aa7053d8ec34502ba8a2f016
Muresan Andrei <murimoro67@gmail.com>
2024-05-02 18:22:48 +0300

    folder resturcturing

--- lib/src/app/main.dart
+++ lib/src/app/main.dart
@@ -1,10 +1,10 @@
-import 'package:app_first_may/splash_page.dart';
+import 'package:app_first_may/src/features/splash/splash_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 
-import 'firebase_options.dart';
+import '../backend/firebase_options.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   try {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app_first_may/main.dart';
+import 'package:app_first_may/src/app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
