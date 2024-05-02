24f4b35fd4e87e81a8e6e5b44d4b347fcab1db18
Victor <victor.craciunas@yahoo.com>
2024-05-02 22:03:50 +0300

    signup page without facebook/google icons added

---
+++ lib/src/constants/texts.dart
@@ -0,0 +1,13 @@
+class Texts {
+
+  static const String signUpTitle = "Create an account";
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+  static const String email = "email";
+  static const String password = "Password";
+  static const String confirmPassword = "Confirm password";
+  static const String iAgreeTo = "I agree to ";
+  static const String privacyPolicy = " Privacy Policy";
+  static const String and = " and ";
+  static const String termsOfUse = "Terms of use ";
+}
